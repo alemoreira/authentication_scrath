7e993a2da9293a4ab5df814f6108b6ed94c82114
Ale <alessandro@alessandro.local>
2014-04-28 20:58:11 -0300

    Criação da Aplicação

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AuthenticationScrath::Application.config.secret_token = '5f018f7491abfb98e6269093c3b01491cccc9e2b58d33fdcb2fe6dfad6e7db0dc51f6df0009a88a99b92c44f8cd910615c476f0584b7566e0f3e0a25d162dfbb'
