ec392749eeab541bc2d3a1efa71407c0e6824bf7
Alessandro Moreira <alessandro.moreira@gmail.com>
2014-04-28 21:00:32 -0300

    Criação da Aplicação

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AuthenticationScrath::Application.config.session_store :cookie_store, key: '_authentication_scrath_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AuthenticationScrath::Application.config.session_store :active_record_store
