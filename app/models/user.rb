246d68e5968b64dd73ea76a1ff5c8bc918877ba8
Alessandro Moreira <alessandro.moreira@gmail.com>
2014-05-05 22:59:57 -0300

    sing_up, sing_in, sing_out

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,22 @@
 class User < ActiveRecord::Base
   attr_accessor :password
 
-  attr_accessible :email, :password_hash, :password_salt
+  attr_accessible :email, :password_hash, :password_salt, :password, :password_confirmation
 
-  validates :email, :presence => true, :uniqueness => true
+  EMAIL_REGEX = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i
+
+  validates :email, :presence => true, :uniqueness => true, :format => EMAIL_REGEX
   validates :password, :presence => true, :on => :create
+  validates_length_of :password, in: 4..16, :on => :create
+  validates :password, :confirmation => true #password_confirmation attr
   
   before_save :encrypt_password
+
+  def self.authenticate email, password
+    # user = self.find_by_email email
+    user = User.find_by_email email
+    user if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+  end
 
   private
 
